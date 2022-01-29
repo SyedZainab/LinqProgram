ebb5c931202613f00f3070671b315e658bb0e7e2
SyedZainab <47735998+SyedZainab@users.noreply.github.com>
2022-01-30 03:46:55 +0530

    Add project files.

---
+++ LinqProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LinqProgram
+{
+    static void Main(string[] args)
+    {
+        string input_str;
+
+        Console.WriteLine("Input the string : ");
+        input_str = Console.ReadLine();
+
+        var Freq = from x in input_str
+                        group x by x into y
+                        select y;
+        Console.Write("The frequency of the characters are :\n");
+        foreach (var ArrElem in Freq)
+        {
+            Console.WriteLine("Character " + ArrElem.Key + ": " + ArrElem.Count() + " times");
+        }
+        Console.ReadLine();
+    }
+}
